ce52f325751da63d554f939712a981bbf1c84c7e
Aaron Barratt <aarontbarratt@gmail.com>
2023-02-08 01:16:52 +0000

    first working example

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ from PIL import Image
 
 current_working_directory = os.getcwd()
 
-# for images in glob.glob(f"{current_working_directory}\\test_images\\*.jpg"):
-#     with Image.open(images) as im:
-#         im.rotate(180).show()
-
-with Image.open("C:\\Users\\aaron\\Documents\\Code\\pyImageCropper\\test_images\\20230125182337_1.jpg") as im:
-    im.crop((960, 0, 2880, 1080)).save(f"output\\{uuid.uuid4()}.jpg")
+for images in glob.glob(f"{current_working_directory}\\test_images\\*.jpg"):
+    with Image.open(images) as im:
+        base_name = os.path.basename(images)
+        im.crop((960, 0, 2880, 1080)).save(f"output\\{base_name}")
