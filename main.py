07e3193eed3555381120e173e35b20331e9d715b
Aaron Barratt <aarontbarratt@gmail.com>
2023-02-11 14:43:16 +0000

    basic ui working

--- main.py
+++ main.py
@@ -1,55 +1,98 @@
-class Cropper:
-    import os
-    import logging
+class PyImageCropper:
     import tkinter as tk
-    from tkinter import filedialog
-    import PIL.Image
+    import logic
+    import sys
+    import logging
 
     def __init__(self):
-        self.repr = "Class to handle the logic of cropping images"
-        self.file_path: str
-        self.working_dir = self.os.getcwd()
-        self.crop_box = (0, 0, 0, 0)
+        self.repr = "Main class for pyImageCropper"
 
-        # create tkinter ui
+        # image processing logic
+        self.image_handler = self.logic.ImageHandler()
+
+        # create UI
         self.window = self.tk.Tk()
-        # this hides the default window
-        self.window.withdraw()
+        self.window.title(self.__class__.__name__)
+        self.window.wm_geometry("500x500")
+        self.window.resizable(False, False)
 
-        # sets the logging level to debug
-        self.logging_flag = True
-        if self.logging_flag:
-            self.logging.getLogger().setLevel(self.logging.DEBUG)
+        # create ui buttons
+        self.button_exit = self.tk.Button(text="Exit", command=self.button_exit_func)
+        self.button_exit.place(x=450, y=0)
 
-        # this should be set in the UI, call function set_crop_box
-        self.set_crop_box((960, 0, 2880, 1080))
-        self.crop_image(self.open_image(), self.get_crop_box())
+        self.button_get_images = self.tk.Button(
+            text="Select", command=self.button_get_images_func
+        )
+        self.button_get_images.place(x=10, y=10)
 
-    def __repr__(self):
-        return self.repr
+        self.button_crop = self.tk.Button(text="Crop", command=self.button_crop_func)
+        self.button_crop.place(x=60, y=10)
 
-    def get_crop_box(self):
-        self.logging.debug(f"get_crop_box: {self.crop_box}")
-        return self.crop_box
+        # TODO: get crop box once and store in a variable. Then allocate values from there
+        # crop box entries
+        self.entry_box_offset = 50
+        # top
+        self.entry_top_string = self.tk.StringVar()
+        self.entry_top_string.set(self.image_handler.get_crop_box()[0])
+        self.entry_top_label = self.tk.Label(text="Top:")
+        self.entry_top_label.place(x=10, y=50)
+        self.entry_top = self.tk.Entry(textvariable=self.entry_top_string)
+        self.entry_top.place(
+            x=int(self.entry_top_label.place_info().get("x")) + self.entry_box_offset,
+            y=int(self.entry_top_label.place_info().get("y")),
+        )
 
-    def set_crop_box(self, box: tuple[int, int, int, int]):
-        self.logging.debug(f"set_crop_box: {box}")
-        self.crop_box = box
+        # left
+        self.entry_left_string = self.tk.StringVar()
+        self.entry_left_string.set(self.image_handler.get_crop_box()[1])
+        self.entry_left_label = self.tk.Label(text="Left:")
+        self.entry_left_label.place(x=10, y=75)
+        self.entry_left = self.tk.Entry(textvariable=self.entry_left_string)
+        self.entry_left.place(
+            x=int(self.entry_left_label.place_info().get("x")) + self.entry_box_offset,
+            y=int(self.entry_left_label.place_info().get("y")),
+        )
 
-    def open_image(self):
-        image_file_path = self.filedialog.askopenfilename(initialdir=self.working_dir)
-        self.logging.debug(f"image_file_path: {image_file_path}")
-        return image_file_path
+        # bottom
+        self.entry_bottom_string = self.tk.StringVar()
+        self.entry_bottom_string.set(self.image_handler.get_crop_box()[2])
+        self.entry_bottom_label = self.tk.Label(text="Bottom:")
+        self.entry_bottom_label.place(x=10, y=100)
+        self.entry_bottom = self.tk.Entry(textvariable=self.entry_bottom_string)
+        self.entry_bottom.place(
+            x=int(self.entry_bottom_label.place_info().get("x"))
+            + self.entry_box_offset,
+            y=int(self.entry_bottom_label.place_info().get("y")),
+        )
 
-    # TODO: get this working with multiple images at the same time
-    def open_images(self):
-        return self.filedialog.askopenfilenames(initialdir=self.working_dir)
+        # right
+        self.entry_right_string = self.tk.StringVar()
+        self.entry_right_string.set(self.image_handler.get_crop_box()[3])
+        self.entry_right_label = self.tk.Label(text="Right:")
+        self.entry_right_label.place(x=10, y=125)
+        self.entry_right = self.tk.Entry(textvariable=self.entry_right_string)
+        self.entry_right.place(
+            x=int(self.entry_right_label.place_info().get("x")) + self.entry_box_offset,
+            y=int(self.entry_right_label.place_info().get("y")),
+        )
 
-    def crop_image(self, image_file_path: str, crop_box: tuple[int, int, int, int]):
-        with self.PIL.Image.open(image_file_path) as im:
-            im = im.crop(crop_box)
-            base_name = self.os.path.basename(image_file_path)
-            im.save(f"output\\{base_name}")
+        # start program loop
+        self.window.mainloop()
+
+    def entry_update_values(self):
+        self.logging.debug(self.entry_top.info)
+
+    def button_get_images_func(self):
+        self.image_handler.set_image_file_paths()
+
+    def button_crop_func(self):
+        self.image_handler.crop_images(
+            self.image_handler.get_image_file_paths(), self.image_handler.get_crop_box()
+        )
+
+    def button_exit_func(self):
+        self.sys.exit()
 
 
-cropper = Cropper()
+if __name__ == "__main__":
+    cropper = PyImageCropper()
